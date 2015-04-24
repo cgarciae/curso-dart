472250d73bfbba4f66a6dc35a46d1f11dcaccfb7
cgarciae <cgarcia.e88@gmail.com>
2015-04-24 14:38:59 -0500

    module

--- bin/testing.dart
+++ bin/testing.dart
@@ -3,17 +3,12 @@ library testing;
 import 'dart:async';
 import 'package:taller/modelos/usuario.dart';
 import 'package:unittest/unittest.dart';
-import 'package:http/http.dart' as http;
-import 'package:redstone/server.dart' as app;
-import 'dart:io' as io;
 import 'package:redstone/mocks.dart';
 import 'package:di/di.dart';
 import 'package:mock/mock.dart';
-import 'package:mongo_dart/mongo_dart.dart';
 import 'package:redstone_mapper_mongo/manager.dart';
 import 'package:redstone_mapper/mapper.dart';
 import 'package:redstone_mapper/mapper_factory.dart';
-import 'package:redstone_mapper/plugin.dart';
 import 'servicios_usuario.dart';
 
 
@@ -83,7 +78,9 @@ main ()
         ..apellido = "Garcia";
       
       //Asignar acciones
-      userServices.when(callsTo('Get')).thenReturn(new Future.value(user));
+      userServices
+        .when(callsTo('Get'))
+        .thenReturn(new Future.value(user));
       
       //Ejecutar acciones
       var usuarioDevuelto = await userServices.Get("");
@@ -97,4 +94,3 @@ main ()
 
 
 class ServiciosUsuarioMock extends Mock implements ServiciosUsuario {}
-
